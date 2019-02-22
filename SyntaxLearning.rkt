5d8dad2a6ad6cc7d46e448831ecf9c3b01920d6c
Sir Snowhorn <18583032+SirSnowHorn@users.noreply.github.com>
2019-02-21 20:20:07 -0600

    Update SyntaxLearning.rkt

--- SyntaxLearning.rkt
+++ SyntaxLearning.rkt
@@ -18,3 +18,31 @@
 ;(let* ([x 1] [y (+ x 1)]) ;Pretty basic stuff. X equals 1, Y equals X plus 1. 
  ;   (writeln(* x y))) ;Now lets print out x times y. 1 * 2 is 2.
  
+#|
+; Recursive code to check if N is even or odd.
+(letrec ([is-even? (lambda (n)
+                        (or (zero? n)
+                            (is-odd? (sub1 n))))]
+            [is-odd? (lambda (n)
+                        (and (not (zero? n))
+                                (is-even? (sub1 n))))])
+    (writeln (is-odd? 11)))
+ |#
+ 
+ #|
+(struct dog (name breed age))
+(define my-pet
+    (dog "charles" "ruff" 9001))
+(writeln my-pet)
+(define (isDog str)
+    (dog? str))
+(writeln (isDog my-pet))
+
+;Let us see if this dog is the OP (overpowered) dog.
+(define (isDogPowerful doggo)
+    (if (> (dog-age doggo) 9000) ;If the age of the dog is over 9000
+        (writeln "Doggo OP!") ;It is OP
+        (writeln "Doggo not OP!"))) ;Otherwise it is not OP
+
+(isDogPowerful my-pet)
+|#
